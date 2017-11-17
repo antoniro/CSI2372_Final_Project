999a26004132561fd6532df3226968393d073897
Oscar Anthony Gbegan <anthony.gbegan@gmail.com>
2017-11-17 18:38:32 -0500

    added dice roll test file

---
+++ DiceRollTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Dice.h"
+
+
+
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		std::cout << "Dice roll " << i + 1 << ": " << RandomDice::distr(RandomDice::generator) << std::endl;
+	}
+
+	std::cin.get();
+}
