7b6b63e73c2dc62034f45ef87ec187338824a2a6
Oscar Anthony Gbegan <anthony.gbegan@gmail.com>
2017-11-18 00:12:44 -0500

    reverted simple Dice.H implementation (old implementation in Dice_old.h file) and added a short test code to test Dice.h

--- Dice.h
+++ Dice.h
@@ -1,45 +1,24 @@
-//
-//  Dice.h
-//  FProject
-//
-//  Created by Arthur Mbouche on 2017-11-16.
-//  Copyright © 2017 Arthur Mbouche. All rights reserved.
-//
-
-#ifndef Dice_h
-#define Dice_h
-
 #pragma once
+
 #include <random>
 #include <iostream>
 #include "Colour.h"
 
-
 struct RandomDice {
-    static std::random_device rDevice;
-   // static std::mt19937 generator;
-   // static std::uniform_int_distribution<int> distr;
-    int random= random_value();
-    static int random_value(){
-        static std::mt19937 mersenne_generator{std::random_device{}()};
-        static std::uniform_int_distribution<int> diceDistribution(1,6);
-        return diceDistribution(mersenne_generator);
-    }
+	static std::random_device rDevice;
+	static std::mt19937 generator;
+	static std::uniform_int_distribution<int> distr;
 };
 
 //Initialize static members of the struct RandomDice
-//std::mt19937 RandomDice::generator(RandomDice::rDevice());
-//std::uniform_int_distribution<int> RandomDice::distr(1, 6);
-
-
+std::random_device RandomDice::rDevice;
+std::mt19937 RandomDice::generator(RandomDice::rDevice());
+std::uniform_int_distribution<int> RandomDice::distr(1, 6);
 
 struct Dice {
-    const Colour colour;
-    int face;
-    RandomDice forRoll;
-    int roll();
-   friend std::ostream& operator<<(std::ostream&, const Dice&);
+	const Colour colour;
+	int face;
+	int roll();
 };
 
 std::ostream& operator<<(std::ostream&, const Dice&);		// Overload insertion operator for printing a Dice to an output stream
-#endif /* Dice_h */
--- DiceRollTest.cpp
+++ DiceRollTest.cpp
@@ -2,8 +2,6 @@
 #include "Dice.h"
 
 
-
-
 int main() {
 	for (int i = 0; i < 10; i++) {
 		std::cout << "Dice roll " << i + 1 << ": " << RandomDice::distr(RandomDice::generator) << std::endl;
