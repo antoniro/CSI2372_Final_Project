e54856b42f11578401c5da01ee907382fd6ecd62
Oscar Anthony Gbegan <anthony.gbegan@gmail.com>
2017-11-18 05:27:00 -0500

    change code in DiceRollTest in order to test for RollOfDice and Dice source code. Runs without error

--- DiceRollTest.cpp
+++ DiceRollTest.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include "Dice.h"
+#include "RollOfDice.h"
 
 
 int main() {
-	for (int i = 0; i < 10; i++) {
-		std::cout << "Dice roll " << i + 1 << ": " << RandomDice::distr(RandomDice::generator) << std::endl;
+	RollOfDice r1;
+	RollOfDice r2;
+	Dice b(Colour::BLUE);
+	Dice r(Colour::RED);
+	Dice g(Colour::GREEN);
+	Dice y(Colour::YELLOW);
+	Dice w(Colour::WHITE);
+
+	r1.addDice(b);
+	r1.addDice(r);
+	r1.addDice(g);
+	r1.roll();
+
+	std::cout << "Dice roll 1: ";
+	for (Dice d : r1) {
+		std::cout << d << "  ";
 	}
+	std::cout << std::endl;
+
+	r1.removeAllDice();
+	r1.addDice(y);
+	r1.addDice(w);
+	r1.roll();
+	std::cout << "Dice roll 2: ";
+	for (Dice d : r1) {
+		std::cout << d << "  ";
+	}
+	std::cout << std::endl;
 
 	std::cin.get();
 }
