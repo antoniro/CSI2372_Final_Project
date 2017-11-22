7e51c04a4cad4cd8c586aee1697b7efdfa3b6fa0
Arthur Mbouche <MBOA@Arthurs-MacBook-Pro.local>
2017-11-21 20:52:43 -0500

    added template definition for validate

--- QwintoRow.h
+++ QwintoRow.h
@@ -1,22 +1,39 @@
+//
+//  QwintoRow.h
+//  FProject
+//
+
+
+#ifndef QwintoRow_h
+#define QwintoRow_h
+
 #pragma once
 
 #include <array>
 #include <vector>
 #include <iostream>
 #include "Colour.h"
+#include "RollOfDice.h"
+#include <stdio.h>
 
 template<Colour C>
 class QwintoRow {
 public:
-	bool validate();
-	int& operator[] (cont int&);
-	friend std::ostream& operator<<(std::ostream&, const QwintoRow<C>&);
-
+    QwintoRow();
+    bool validate(const int&);
+    int& operator[] (const int&);
+    friend std::ostream& operator<<(std::ostream& os, const QwintoRow< C>& );
+    
 private:
-	int invalidIndex;
-	std::vector<int> bonusIndex;
-	std::array<int, 10> row;
+    int invalidIndex;
+    std::vector<int> bonusIndex;
+    std::array<int, 10> row;
 };
 
 template<Colour C>
-std::ostream& operator<<(std::ostream&, const QwintoRow<C>&);
+std::ostream& operator<<(std::ostream&, const QwintoRow<C>& );
+
+template<Colour C>
+bool validate(const int&);
+
+#endif /* QwintoRow_h */
