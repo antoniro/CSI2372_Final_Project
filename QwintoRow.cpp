8254736330b2e20536f37c845294b9f7ee7d6111
Arthur Mbouche <MBOA@Arthurs-MacBook-Pro.local>
2017-11-21 21:05:02 -0500

    new class QwintoRow.cpp

---
+++ QwintoRow.cpp
@@ -0,0 +1,41 @@
+//
+//  QwintoRow.cpp
+//  FProject
+//
+
+
+#include "QwintoRow.h"
+
+//https://www.codeproject.com/Articles/48575/How-to-define-a-template-class-in-a-h-file-and-imp
+
+template<Colour C>
+QwintoRow<C>::QwintoRow(): invalidIndex(-1),bonusIndex(100){
+    for(int j=0;j<row.max_size();++j){
+        row[j]=0;
+    }
+}
+
+
+template<Colour C>
+int& QwintoRow<C>::operator[] (const int& number){
+    return row[number];
+}
+
+template<Colour C>
+bool QwintoRow<C>::validate(const int& index){
+    if(index >= 0 && index < row.max_size()) return true;
+    return false;
+}
+
+//not sure about this
+template<Colour C>
+std::ostream& operator<<(std::ostream& os, const QwintoRow<C> & qRow ){
+  
+    for(int s=0; s< qRow.row.max_size(); s++){
+        os<< qRow[s] << "| ";
+    }
+    
+    
+    return os;
+}
+
