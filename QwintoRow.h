8b7584dbfe2aff9f540528258d8172755dde1849
Arthur Mbouche <MBOA@Arthurs-MacBook-Pro.local>
2017-11-25 18:21:59 -0500

    implementing the template functions in the header. Doing this in the cpp file caussed linker errors for especially for the constructor

--- QwintoRow.h
+++ QwintoRow.h
@@ -3,28 +3,51 @@
 //  FProject
 //
 
-
+#pragma once
 #ifndef QwintoRow_h
 #define QwintoRow_h
-
-#pragma once
 
 #include <array>
 #include <vector>
 #include <iostream>
-#include "Colour.h"
 #include "RollOfDice.h"
+#include <iomanip>
 
 
 template<Colour C>
 class QwintoRow {
 public:
-    QwintoRow();
+    QwintoRow<C>();
     bool validate(const int&);
     int& operator[] (const int&);
+   int maxSize( )const{return row.max_size();}
+  //  int size() const{return row.size();}
     
-    template<Colour D>
-    friend std::ostream& operator<<(std::ostream& , const QwintoRow<D>& );
+   
+    friend std::ostream& operator<<(std::ostream& os, const QwintoRow<C>& qRow){
+        
+        switch(C){
+            case Colour::RED:
+        os<<std::setw(2)<< "| "<<qRow.row[0]<<" % "<<qRow.row[1]<<" % "
+        <<qRow.row[2]<<" | "<<"XX"<<"| "<<qRow.row[4]<<" % "<<qRow.row[5]<<" % "<<qRow.row[6]<<" | "
+        <<qRow.row[7]<<" | "<<qRow.row[8]<<" | "<<qRow.row[9]<<" | " ;
+                break;
+            case Colour::YELLOW:
+                os<<std::setw(2)<< "| "<<qRow.row[0]<<" | "<<qRow.row[1]<<" | "
+                <<qRow.row[2]<<" | "<< qRow.row[3]<<" | "<<qRow.row[4]<<" | "<<"XX"<<"| "<<qRow.row[6]<<" % "
+                <<qRow.row[7]<<" % "<<qRow.row[8]<<" | "<<qRow.row[9]<<" | " ;
+                break;
+            case Colour::BLUE:
+                os<<std::setw(2)<< "| "<<qRow.row[0]<<" | "<<qRow.row[1]<<" % "
+                <<qRow.row[2]<<" % "<<qRow.row[3]<<" | "<<"XX"<<"| "<<qRow.row[5]<<" | "<<qRow.row[6]<<" | "
+                <<qRow.row[7]<<" | "<<qRow.row[8]<<" % "<<qRow.row[9]<<" % " ;
+                break;
+            default: break;
+        }
+        return os;
+    }
+    
+
         
     
     
@@ -32,15 +55,72 @@ private:
     int invalidIndex;
     std::vector<int> bonusIndex;
     std::array<int, 10> row;
+    int tally;
+    
+    
 };
 
 template<Colour C>
 std::ostream& operator<<(std::ostream& os, const QwintoRow<C>& );
-  // os<<"c";
-   // return os;
-//}
 
 template<Colour C>
 bool validate(const int&);
+
+
+template <Colour D>
+ QwintoRow<D>::QwintoRow(): invalidIndex(-1),bonusIndex(100){
+    
+    switch(D){
+        case Colour::RED:
+            for(int j=0;j<row.size();++j){
+                if(j==3){
+                    row[j]=invalidIndex;
+                }
+                else{
+                    row[j]=0;
+                }
+            }
+            break;
+        case Colour::YELLOW:
+            for(int j=0;j<row.size();++j){
+                if(j==5){
+                    row[j]=invalidIndex;
+                        }
+                else{
+                    row[j]=0;
+                }
+            }
+            break;
+        case Colour::BLUE:
+            for(int j=0;j<row.size();++j){
+                if(j==4){
+                    row[j]=invalidIndex;
+                }
+                else{
+                    row[j]=0;
+                }
+                
+            }
+            break;
+        default: break;
+    }
+}
+
+
+template<Colour C>
+int& QwintoRow<C>::operator[] (const int& number){ 
+    return row[number];
+}
+ 
+template<Colour C>
+bool QwintoRow<C>::validate(const int& index){
+    if(index < 0 || index > row.max_size()) {
+        return false;
+         }
+    if(row[index]==0) {return true;}
+    else{
+        return false;
+    }
+}
 
 #endif /* QwintoRow_h */
