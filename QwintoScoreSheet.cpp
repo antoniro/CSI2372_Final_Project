b57c848c06429ab04855ff2cd24bb7b83f92ef7c
Arthur Mbouche <MBOA@Arthurs-MacBook-Pro.local>
2017-12-01 22:39:21 -0500

    Implemented bonus computation, not fully tested

--- QwintoScoreSheet.cpp
+++ QwintoScoreSheet.cpp
@@ -6,8 +6,11 @@
 
 #include "QwintoScoreSheet.h"
 
+int QwintoScoreSheet::getTotal() const{
+    return finalTotal;
+}
 
-QwintoScoreSheet::QwintoScoreSheet(std::string nName): ScoreSheet(nName) ,tally(0), detCol(false) , redTally(0),blueTally(0),yellowTally(0),finalTotal(0){
+QwintoScoreSheet::QwintoScoreSheet(std::string nName): ScoreSheet(nName) ,tally(0) , redTally(0),blueTally(0),yellowTally(0),finalTotal(0){
    }
 
 void QwintoScoreSheet::setTotal(){
@@ -16,15 +19,52 @@ void QwintoScoreSheet::setTotal(){
 
 int QwintoScoreSheet::calcTotal() {
     int total=0;
-    int finalTotal=0;
+    int redBonus=1;
+    int yellowBonus=7;
+    int blueBonus=2;
     
+    total = numPointsRow();
+    // bonus computation is missing
+    total += computeBonus(Colour::RED, redBonus);
+    redBonus+=5;
+    total += computeBonus(Colour::RED, redBonus);
+    total += computeBonus(Colour::YELLOW, yellowBonus);
+    total += computeBonus(Colour::BLUE, blueBonus);
+    blueBonus+=5;
+    total += computeBonus(Colour::BLUE, blueBonus);
+    total=total-(getFailCount()*5);
+    
+    return total;
+}
+
+int QwintoScoreSheet::computeBonus(Colour c, int z) {
+    int score=0;
+    switch(c){
+        case Colour::RED:
+            if(red[z]>0 && (yellow[z+1]>0) && (blue[z+2]>0)) return score= red[z];
+            return 0;
+            break;
+        case Colour::YELLOW:
+            if((yellow[z]>0) && (red[z-1]>0) && (yellow[z+1]>0)) return score= yellow[z];
+            return 0;
+            break;
+        case Colour::BLUE:
+            if((blue[z]>0) && (yellow[z-1]>0) && (red[z-2]>0)) return score= blue[z];
+            return 0;
+            break;
+        default:break;
+    }
+return score;
+}
+
+
+int  QwintoScoreSheet::numPointsRow(){
+    int total=0;
     //go through each row and calculate points
     //red row
-    
-    if(redTally==red.maxSize()){
+    if(redTally==red.maxSize()){  //check full row
         total+=red[red.maxSize()-1];
     }else{ total+=redTally;}
-    
     if(yellowTally==yellow.maxSize()){
         total+=yellow[yellow.maxSize()-1];
     }else{ total+=yellowTally;}
@@ -32,35 +72,27 @@ int QwintoScoreSheet::calcTotal() {
     if(blueTally==blue.maxSize()){
         total+=blue[blue.maxSize()-1];
     }else{ total+=blueTally; }
-    
-    // bonus computation is missing
-    
-    total=total-(getFailCount()*5);
-    
-    return total;
+return total;
 }
 
 bool QwintoScoreSheet::score(RollOfDice& rd,  Colour & newColor, int z){
-    int j = rd;
-    rd.roll();
-    bool score=validate(newColor,z);
+   // int j = rd;
+   // rd.roll();
+    bool score=validate(newColor,rd,z);
     if(score){
         switch(newColor){
             case Colour::RED:
                 red[z]=rd;
                 redTally++;
-                //std::cout<<"here"<<j<<"\n";
                 break;
             case Colour::YELLOW:
                 yellow[z]=rd;
                 yellowTally++;
-                //std::cout<<"not yellow here";
                 break;
             case Colour::BLUE:
                 blue[z]=rd;
-                std::cout<<rd;
+               // std::cout<<rd;
                 blueTally++;
-                //std::cout<<"not blue here";
                 break;
             default: break;
         }
@@ -73,17 +105,18 @@ bool QwintoScoreSheet::score(RollOfDice& rd,  Colour & newColor, int z){
 
 
 std::ostream  & QwintoScoreSheet::print(std::ostream & toStream){
+    std::cout<<"\n";
     toStream<< "Player name: "<< playerName << "    Points:"<<"\n";
-    toStream<< "                 __________________________________________"<<"\n";
+    toStream<< "                     ___________________________________________________"<<"\n";
     toStream<<""<<"\n";
     toStream<< "Red"<<"                 "<<red<<"\n";
-    toStream<< "             ______________________________________________"<<"\n";
+    toStream<< "                ________________________________________________________"<<"\n";
     toStream<<""<<"\n";
-    toStream<< "Yellow"<<"           "<<yellow<<"\n";
-    toStream<< "             __________________________________"<<"\n";
+    toStream<< "Yellow"<<"         "<<yellow<<"\n";
+    toStream<< "           ________________________________________________________"<<"\n";
     toStream<<""<<"\n";
-    toStream<< "Blue"<<"          "<<blue<<"\n";
-    toStream<< "             _______________________________"<<"\n";
+    toStream<< "Blue"<<"      "<<blue<<"\n";
+    toStream<< "           ___________________________________________________"<<"\n";
     toStream<<""<<"\n";
     toStream<< "Failed Throws: ";
     printCount(std::cout);
@@ -93,10 +126,10 @@ std::ostream  & QwintoScoreSheet::print(std::ostream & toStream){
     return toStream;
 }
 
- bool QwintoScoreSheet::validate(Colour & qwinto, int & dice){
+ bool QwintoScoreSheet::validate(Colour & qwinto,RollOfDice & rd ,int & dice){
     switch(qwinto){
         case Colour::RED:
-           detCol= red.validate(dice);
+           detCol= red.validate(dice);  
             break;
         case Colour::YELLOW:
           detCol=  yellow.validate(dice);
@@ -115,28 +148,28 @@ bool QwintoScoreSheet::operator!() const {
     if(tally>=2) return true;
     return false;
 }
-std::ostream & QwintoScoreSheet::printCount(std::ostream & toStream) const{
+std::ostream & QwintoScoreSheet::printCount(std::ostream & toStream) const{ 
     for(int loop=1;loop<getFailCount()+1;loop++){
         toStream<<loop<<" ";
     }
     return toStream;
-}
+} 
 
-std::ostream& operator<<(std::ostream& os, const QwintoScoreSheet& qwintoS){
-    std::cout<< "Player name: "<<std::endl;
-    std::cout<< "                   _________________________________________"<<std::endl;
+std::ostream& operator<<(std::ostream& os, const QwintoScoreSheet& qwintoS){ 
+    std::cout<<"Player name: "<< qwintoS.getPlayerName()<<std::endl;
+    std::cout<< "                      ___________________________________________________"<<std::endl;
+    std::cout<<""<<std::endl;   
+    std::cout<< "Red"<<"                  "<<qwintoS.red<<std::endl;
+    std::cout<< "                 ________________________________________________________"<<std::endl;
     std::cout<<""<<std::endl;
-    std::cout<< "Red"<<"                "<<qwintoS.red<<std::endl;
-    std::cout<< "               _____________________________________________"<<std::endl;
-    std::cout<<""<<std::endl;
-    std::cout<< "Yellow"<<"         "<<qwintoS.yellow<<std::endl;
-    std::cout<< "           _____________________________________________"<<std::endl;
+    std::cout<< "Yellow"<<"          "<<qwintoS.yellow<<std::endl;
+    std::cout<< "            ________________________________________________________"<<std::endl;
     std::cout<<""<<std::endl;
     std::cout<< "Blue"<<"       "<<qwintoS.blue<<std::endl;
-    std::cout<< "           _________________________________________"<<std::endl;
+    std::cout<< "            ___________________________________________________"<<std::endl;
     std::cout<<""<<std::endl;
     std::cout<< "Failed Throws:";
-    return os;
+    return os; 
 }
 
 
--- QwintoScoreSheet.h
+++ QwintoScoreSheet.h
@@ -7,16 +7,17 @@
 #ifndef QwintoScoreSheet_h
 #define QwintoScoreSheet_h
 
-
 #include "ScoreSheet.h"
 
 //QwintoScoresheet Class
 class QwintoScoreSheet: public ScoreSheet{
-public:   QwintoRow<Colour::RED> red;
+public:
+    QwintoRow<Colour::RED> red;
     QwintoRow<Colour::BLUE> blue;
     QwintoRow<Colour::YELLOW> yellow;
 public:
    // using ScoreSheet::ScoreSheet;
+    int getTotal() const;
    QwintoScoreSheet(std::string name);
     virtual void setTotal() override;
     virtual bool operator!() const override;
@@ -26,14 +27,16 @@ public:
     friend std::ostream& operator<<(std::ostream&, const ScoreSheet&);
     
     std::ostream & printCount(std::ostream &) const;
+    int  numPointsRow();
+    int computeBonus(Colour , int );
     
 protected:
     
     virtual int calcTotal() override;
-   virtual bool validate(Colour  & a, int & valid) override;
+   virtual bool validate(Colour  & , RollOfDice &, int  & x ) override;
     int tally;
     int redTally;
-    int yellowTally;
+    int yellowTally;  // keep track of number of dice scored in sheet for later score computation
     int blueTally;
     bool detCol;
     int finalTotal;
