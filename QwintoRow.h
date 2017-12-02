d4850ea6c1fd3c19f5b0560157cf9c4733c5a8ea
Arthur Mbouche <MBOA@Arthurs-MacBook-Pro.local>
2017-12-01 19:41:48 -0500

    Fixed the printing of zeros in place of space in friend insert operator

--- QwintoRow.h
+++ QwintoRow.h
@@ -22,38 +22,89 @@ public:
     int& operator[] (const int&);
    int maxSize( )const{return row.max_size();}
   //  int size() const{return row.size();}
-    
+    void initialize(int invalid);
    
     friend std::ostream& operator<<(std::ostream& os, const QwintoRow<C>& qRow){
-        
-        switch(C){
+        os<<std::setw(2);
+        switch(C){ 
             case Colour::RED:
-        os<<std::setw(2)<< "| "<<qRow.row[0]<<" % "<<qRow.row[1]<<" % "
-        <<qRow.row[2]<<" | "<<"XX"<<"| "<<qRow.row[4]<<" % "<<qRow.row[5]<<" % "<<qRow.row[6]<<" | "
-        <<qRow.row[7]<<" | "<<qRow.row[8]<<" | "<<qRow.row[9]<<" | " ;
+                for(int z=0; z<qRow.row.size(); z++){
+                    if((z==1)||(z==5) ||(z==2)||(z==6)){
+                        os<<std::setw(2)<<" % ";
+                        if( qRow.row[z]==0){
+                          os<<std::setw(2)<<"";
+                        }
+                        else{
+                            os<<std::setw(2)<<qRow.row[z];
+                        }
+                    }
+                    else{
+                        os<<std::setw(2)<<" | ";
+                        if(qRow.row[z]==-1){
+                            os<<"XX";
+                        }else if(qRow.row[z]==0){
+                            os<<std::setw(2)<<"";
+                        }else{
+                            os<<std::setw(2)<<qRow.row[z];
+                        }
+                    }
+                }
+                os<<" | ";
                 break;
             case Colour::YELLOW:
-                os<<std::setw(2)<< "| "<<qRow.row[0]<<" | "<<qRow.row[1]<<" | "
-                <<qRow.row[2]<<" | "<< qRow.row[3]<<" | "<<qRow.row[4]<<" | "<<"XX"<<"| "<<qRow.row[6]<<" % "
-                <<qRow.row[7]<<" % "<<qRow.row[8]<<" | "<<qRow.row[9]<<" | " ;
+                for(int z=0; z<11 ; z++){
+                    if(z==7||(z==8)){
+                        os<<std::setw(2)<<" % ";
+                        if( qRow.row[z]==0){
+                            os<<std::setw(2)<<"";
+                        }
+                        else{
+                            os<<std::setw(2)<<qRow.row[z];
+                        }
+                    }
+                    else{
+                        os<<std::setw(2)<<" | ";
+                        if(qRow.row[z]==-1){
+                            os<<"XX";
+                        }else if(qRow.row[z]==0){
+                            os<<std::setw(2)<<"";
+                        }else{
+                            os<<std::setw(2)<<qRow.row[z];
+                        }
+                    }
+                }
                 break;
             case Colour::BLUE:
-                os<<std::setw(2)<< "| "<<qRow.row[0]<<" | "<<qRow.row[1]<<" % "
-                <<qRow.row[2]<<" % "<<qRow.row[3]<<" | "<<"XX"<<"| "<<qRow.row[5]<<" | "<<qRow.row[6]<<" | "
-                <<qRow.row[7]<<" | "<<qRow.row[8]<<" % "<<qRow.row[9]<<" % " ;
+                for(int z=0; z<qRow.row.size()+1; z++){
+                    if((z==2)||(z==3) ||(z==9)||(z==10)){
+                        os<<std::setw(2)<<" % ";
+                        if( qRow.row[z]==0){
+                            os<<std::setw(2)<<" ";
+                        }
+                        else{
+                            os<<std::setw(2)<<qRow.row[z];
+                        }
+                    }
+                    else{
+                        os<<std::setw(2)<<" | ";
+                        if(qRow.row[z]==-1){
+                            os<<"XX";
+                        }
+                        else if(qRow.row[z]==0){
+                             os<<std::setw(2)<<"";
+                        }else{
+                             os<<std::setw(2)<<qRow.row[z];
+                        }
+                    }
+                }
                 break;
             default: break;
         }
         return os;
     }
-    
-
         
-    
-    
 private:
     int invalidIndex;
-    std::vector<int> bonusIndex;
     std::array<int, 10> row;
     int tally;
     
@@ -61,60 +112,51 @@ private:
 };
 
 template<Colour C>
+void QwintoRow<C>::initialize(int invalid){
+    for(int j=0;j<row.size();++j){
+        if(j==invalid){
+            row[invalid]=invalidIndex;
+        }
+        else{
+            row[j]=0;
+        }
+    }
+}
+
+
+template<Colour C>
 std::ostream& operator<<(std::ostream& os, const QwintoRow<C>& );
 
 template<Colour C>
 bool validate(const int&);
 
-
+ 
 template <Colour D>
- QwintoRow<D>::QwintoRow(): invalidIndex(-1),bonusIndex(100){
+ QwintoRow<D>::QwintoRow(): invalidIndex(-1){
     
     switch(D){
         case Colour::RED:
-            for(int j=0;j<row.size();++j){
-                if(j==3){
-                    row[j]=invalidIndex;
-                }
-                else{
-                    row[j]=0;
-                }
-            }
+            initialize(3);
             break;
         case Colour::YELLOW:
-            for(int j=0;j<row.size();++j){
-                if(j==5){
-                    row[j]=invalidIndex;
-                        }
-                else{
-                    row[j]=0;
-                }
-            }
-            break;
+             initialize(5);
+                        break;
         case Colour::BLUE:
-            for(int j=0;j<row.size();++j){
-                if(j==4){
-                    row[j]=invalidIndex;
-                }
-                else{
-                    row[j]=0;
-                }
-                
-            }
-            break;
+             initialize(4);
+            break;  
         default: break;
     }
 }
 
 
 template<Colour C>
-int& QwintoRow<C>::operator[] (const int& number){ 
+int& QwintoRow<C>::operator[] (const int& number){  
     return row[number];
 }
  
 template<Colour C>
 bool QwintoRow<C>::validate(const int& index){
-    if(index < 0 || index > row.max_size()) {
+    if(index < 0 || index > row.max_size()) {   
         return false;
          }
     if(row[index]==0) {return true;}
