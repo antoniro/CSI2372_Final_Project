37cdf63c2de99d22741b52b434d401ac7fb5f55a
Arthur Mbouche <MBOA@Arthurs-MBP.lan>
2017-12-02 03:59:30 -0500

    removed const in colour member, this was hindering std::vector function-erase

--- Dice.cpp
+++ Dice.cpp
@@ -7,16 +7,17 @@ std::uniform_int_distribution<int> RandomDice::distr(1, 6);
 
 int Dice::roll()
 {
-	face = RandomDice::distr(RandomDice::generator);
-	return face;
+    face = RandomDice::distr(RandomDice::generator);
+    return face;
 }
 
-Dice::Dice(Colour const col) : colour{col}
+Dice::Dice( const Colour col ) : colour(col)
 {
 }
 
 std::ostream & operator<<(std::ostream & os, Dice const & dice)
 {
-	os << dice.face;
-	return os;
+    os << dice.face;
+    return os;
 }
+
--- Dice.h
+++ Dice.h
@@ -5,16 +5,16 @@
 #include "Colour.h"
 
 struct RandomDice {
-	static std::random_device rDevice;
-	static std::mt19937 generator;
-	static std::uniform_int_distribution<int> distr;
+    static std::random_device rDevice;
+    static std::mt19937 generator;
+    static std::uniform_int_distribution<int> distr;
 };
 
 struct Dice {
-	Colour const colour;
-	int face;
-	int roll();
-	Dice(Colour const);
+    Colour colour;
+    int face;
+    int roll();
+    Dice( const Colour);
 };
 
-std::ostream& operator<<(std::ostream&, Dice const&);		// Overload insertion operator for printing a Dice to an output stream
+std::ostream& operator<<(std::ostream&, Dice const &);
